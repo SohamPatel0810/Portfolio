513b14035bb51d35dc0b5c3bdde211c673174d04
Soham <sohampatel2050@gmail.com>
2024-03-12 02:06:19 -0400

    minor bug fix in animation of projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {Tilt} from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -17,15 +16,8 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full hover:scale-100'
-      >
+    <motion.div >
+      <div className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full hover:scale-100'>
         <div className='relative w-full h-[230px]'>
           <img
             src={image}
@@ -62,7 +54,7 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-      </Tilt>
+      </div>
     </motion.div>
   );
 };
@@ -70,14 +62,14 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
       <div className='w-full flex'>
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+          // variants={fadeIn("left", "spring", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           Following projects showcases my skills and experience through
