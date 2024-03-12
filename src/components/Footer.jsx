62c568a93fcd07ac398c446d3b611d8e98f983cf
Soham <sohampatel2050@gmail.com>
2024-03-12 02:12:41 -0400

    minor color change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
 
     <div className='flex flex-col justify-end  mt-5'>
-      <p style={{fontSize:"6px"}}>Icons of aws,mysql,python,externallink by Icons8 | Alba Stepup :- https://alba-ewaste.sg/earn-rewards/ | Kabata :- https://www.kabatafitness.com/ | Equester:- https://www.equester.ae/   </p>
+      <p style={{fontSize:"6px",color:"black"}}>Icons of aws,mysql,python,externallink by Icons8 | Alba Stepup :- https://alba-ewaste.sg/earn-rewards/ | Kabata :- https://www.kabatafitness.com/ | Equester:- https://www.equester.ae/   </p>
     </div>
 
 
