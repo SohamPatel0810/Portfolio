2464f9d68b14d97b4f5563f76cdd6c566232118d
Soham <sohampatel2050@gmail.com>
2024-12-05 17:12:49 -0500

    bug resolved

--- src/assets/index.js
+++ src/assets/index.js
@@ -24,6 +24,7 @@ import python from "./tech/python.svg";
 
 import fai from "./company/fai.jpeg";
 import openxcell from "./company/openxcell.jpeg";
+import clevertech_logo from "./company/clevertech_logo.jpg";
 
 import sensor from "./sensor.png";
 import alba from "./alba.png";
@@ -56,6 +57,7 @@ export {
   reactjs,
   tailwind,
   fai,
+  clevertech_logo,
   openxcell,
   sensor,
   chatmate,
